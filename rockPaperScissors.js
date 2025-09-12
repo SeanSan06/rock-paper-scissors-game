a443db2ac5cafb49a613edeaabb6faf7628d4eee
Sean San <seanksan@gmail.com>
2025-09-12 14:01:08 -0700

    Add logic to play one round and find winner

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,3 +1,7 @@
+let computerScore = 0;
+let humanScore = 0;
+
+// Helper functions
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
     if(randomNumber == 1) {
@@ -10,5 +14,49 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    prompt("Choose rock, paper, or scissor");
+    let userInput = prompt("Choose rock, paper, or scissor");
+
+    return userInput;
 }
+
+function playRound(computerChoice, humanChoice) {
+    computerChoice = computerChoice.toLowerCase();
+    humanChoice = humanChoice.toLowerCase();
+
+    if(computerChoice == "rock") {
+        if(humanChoice == "rock") {
+            console.log("Tie");
+        } else if(humanChoice == "paper") {
+            console.log("Human wins");
+            ++humanScore;
+        } else if(humanChoice == "scissor") {
+            console.log("Computer wins");
+            ++computerScore;
+        }
+    } else if((computerChoice == "paper")) {
+        if(humanChoice == "rock") {
+            console.log("Computer wins");
+            ++computerScore;
+        } else if(humanChoice == "paper") {
+            console.log("Tie");
+        } else if(humanChoice == "scissor") {
+            console.log("Human wins");
+            ++humanScore;
+        }
+    } else if((computerChoice == "scissor")) {
+        if(humanChoice == "rock") {
+            console.log("Human wins");
+            ++humanScore;
+        } else if(humanChoice == "paper") {
+            console.log("Computer wins");
+            ++computerScore;
+        } else if(humanChoice == "scissor") {
+            console.log("Tie");
+        }
+    }
+}
+
+// Main game loop WIP
+computer = getComputerChoice();
+human = getHumanChoice()
+playRound(x, y);
