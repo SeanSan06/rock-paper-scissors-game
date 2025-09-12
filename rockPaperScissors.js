75d3d6543c8ea4ca51e77e2afecc1e772f1ba838
Sean San <seanksan@gmail.com>
2025-09-12 13:11:04 -0700

    Add randomized function to get computer choice
    The function allows a 'computer' to randomly pick rock, paper, or
    scissors.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,1 +1,10 @@
-console.log("Testing JS connects to html");
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    if(randomNumber == 1) {
+        return "rock";
+    } else if(randomNumber == 2) {
+        return "paper";
+    } else {
+        return "scissor";
+    }
+}
